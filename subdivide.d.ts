e665f22b546abf0f80d8a8b16e9513707873f169
Kevin Dangoor <kevindangoor@khanacademy.org>
2016-06-05 00:33:59 -0400

    Change to use built-in store to avoid endless loop.

--- subdivide.d.ts
+++ subdivide.d.ts
@@ -3,12 +3,12 @@ import {Dispatch} from "redux";
 
 interface SubdivideProps {
     DefaultComponent: {new(...args: any[]): React.Component<any, any>};
-    dispatch: Dispatch;
-    subdivide: any;
     componentProps: any;
 }
 
-declare class Subdivide extends React.Component<SubdivideProps, any> {}
+declare class Subdivide extends React.Component<SubdivideProps, any> {
+	store: any;
+}
 
 export function reducer(state: any, action: any): any;
 
